8bdc31635b18626f1053f3a5d94e332919a33594
Sander Gaarden <sandergaarden@gmail.com>
2021-04-26 11:43:38 +0200

    Updated to fit the Camera library.

--- CameraLibTest/CameraLibTest.ino
+++ CameraLibTest/CameraLibTest.ino
@@ -2,12 +2,22 @@
 
 #include "Camera.h"
 
+Camera camera;
+
+angleSet angles = { angles.xAngle = 90, angles.yAngle = 180 };
+angleSet angles2 = { angles2.xAngle = 0, angles.yAngle = 90 };
+angleSet angles3 = { angles3.xAngle = 180, angles.yAngle = 0 };
+
 void setup() {
   Serial.begin(115200);
-  servoSetup();
+  delay(3000);
 }
 
 void loop() {
+  camera.move(angles);
   delay(1500);
-  testServos();
+  camera.move(angles2);
+  delay(1500);
+  camera.move(angles3);
+  delay(1500);
 }

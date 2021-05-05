d49f4d7d5dd510acfe4bdd3d147edeaa2186bbd0
Sander Gaarden <sandergaarden@gmail.com>
2021-05-05 14:22:55 +0200

    Moved class-based version to "Deprecated" folder, made something that actually works.

--- Deprecated/CameraLibTest/CameraLibTest.ino
+++ Deprecated/CameraLibTest/CameraLibTest.ino
@@ -2,21 +2,22 @@
 
 #include "Camera.h"
 
+Camera camera;
+
 angleSet angles = { angles.xAngle = 90, angles.yAngle = 180 };
 angleSet angles2 = { angles2.xAngle = 0, angles.yAngle = 90 };
 angleSet angles3 = { angles3.xAngle = 180, angles.yAngle = 0 };
 
 void setup() {
   Serial.begin(115200);
-  cameraSetup();
   delay(3000);
 }
 
 void loop() {
-  move(angles);
+  camera.move(angles);
   delay(1500);
-  move(angles2);
+  camera.move(angles2);
   delay(1500);
-  move(angles3);
+  camera.move(angles3);
   delay(1500);
 }

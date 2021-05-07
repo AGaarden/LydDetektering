76859de2a6d4b8dc3e799878184b811be97c1360
Sander Gaarden <sandergaarden@gmail.com>
2021-05-07 12:14:47 +0200

    Various test cases added.

--- CameraLibTestPIO/src/main.cpp
+++ CameraLibTestPIO/src/main.cpp
@@ -4,9 +4,11 @@
 
 Camera camera;
 
-angleSet angles = { angles.xAngle = 90, angles.yAngle = 180 };
-angleSet angles2 = { angles2.xAngle = 0, angles.yAngle = 90 };
-angleSet angles3 = { angles3.xAngle = 180, angles.yAngle = 0 };
+angleSet resetAngles = { resetAngles.xAngle = 0, resetAngles.yAngle = 0};
+
+angleSet angles = { angles.xAngle = 0, angles.yAngle = 0 };
+angleSet angles2 = { angles2.xAngle = 90, angles.yAngle = 90 };
+angleSet angles3 = { angles3.xAngle = 180, angles.yAngle = 180 };
 
 void setup() {
   Serial.begin(115200);
@@ -14,10 +16,16 @@ void setup() {
 }
 
 void loop() {
+  
   camera.move(angles);
   delay(1500);
   camera.move(angles2);
   delay(1500);
   camera.move(angles3);
   delay(1500);
+  
+
+ // camera.move(resetAngles);
+
+  // camera.testServos();
 }

2fea2a3b4911942c647b8e1ed4c3ea0d322ef7d6
MarkusEllyton <markusellyton@gmail.com>
2021-05-07 13:16:39 +0200

    Added new directory with updated Camera-library

---
+++ FFTTimeshift_Camera/lib/eventdetect/eventdetect.h
@@ -0,0 +1,37 @@
+#include <Arduino.h>
+#include <stdio.h>
+#include <driver/i2s.h>
+#include <driver/adc.h>
+#include <soc/syscon_reg.h>
+#include <soc/i2s_reg.h>
+#include <soc/dport_reg.h>
+#include <soc/dport_access.h>
+#include <driver/periph_ctrl.h>
+#include <fft.h>
+
+#define DCOFFSET 1.3
+#define BUF_CNT 8
+#define BUF_LEN 1024
+#define ADC_LEN ((1024*8)/4)
+
+#define I2S0_CLKM_CONF_REG 0x3FF4F0AC
+#define I2S0_SAMPLE_RATE_CONF_REG 0x3FF4F0B0
+
+const float upper_threshold = 0.3;
+const float lower_threshold = -0.3;
+
+typedef enum {
+    REAL_FORWARD,
+    REAL_BACKWARD
+} fft_type_dir;
+
+void i2s_init();
+bool sample_checkamplitude(float *buff0, float *buff1, float *buff2, float *buff3);
+int16_t calc_sample_shift(int32_t *pBuf1, int32_t *pBuf2);
+void corr_s32(const int32_t *Signal, const int siglen, const int32_t *Pattern, const int patlen, int32_t *dest);
+void real_fft(fft_config_t *fft_plan, float *input_arr);
+void conj_array(fft_config_t *fft_plan);
+void convolve(fft_config_t *fft_plan_0, fft_config_t *fft_plan_1);
+float fft_timeshift(float *arr_0, float *arr_1);
+int argmax(fft_config_t *fft_plan, float *input_array);
+float arg_to_sec(int arg, int fs);
---
+++ FFTTimeshift_Camera/src/main.cpp
@@ -0,0 +1,54 @@
+#include <Arduino.h>
+#include <eventdetect.h>
+#include <direction.h>
+#include <math.h>
+
+static float adc0buff[2048];
+static float adc3buff[2048];
+static float adc6buff[2048];
+static float adc7buff[2048];
+
+unsigned int start_b, end_b;        // Variable for execution measurement
+angleSet angle;
+Camera camera;
+
+void setup() {
+  Serial.begin(921600);
+  i2s_init();
+
+  Serial.println("System started...");
+}
+
+void loop() {
+  while (1) {
+
+    if (sample_checkamplitude(&adc0buff[0], &adc3buff[0], &adc6buff[0], &adc7buff[0])) {
+      float shift0_3, shift0_6, shift0_7;
+
+      start_b = esp_timer_get_time();          
+
+      shift0_3 = fft_timeshift(&adc0buff[0], &adc3buff[0]);
+      shift0_6 = fft_timeshift(&adc0buff[0], &adc6buff[0]);
+      shift0_7 = fft_timeshift(&adc0buff[0], &adc7buff[0]);
+
+      // // Initiate sample shift calculation on the existing buffers.
+      // shift0_3 = (float) (calc_sample_shift(&adc0buff[0], &adc3buff[0]))*0.00002;
+      // shift0_6 = (float) (calc_sample_shift(&adc0buff[0], &adc6buff[0]))*0.00002;
+      // shift0_7 = (float) (calc_sample_shift(&adc0buff[0], &adc7buff[0]))*0.00002;
+      // angle = direction(shift0_3, shift0_6, shift0_7);
+      // camera.move(angle);
+      end_b = esp_timer_get_time();  
+
+      //printf("Angle \txy: %f \tyz: %f\n", angle.xAngle, angle.yAngle);
+      //printf("[Detected] 0-3: %.6f s \t 0-6: %.6f s \t 0-7: %.6f s \n", shift0_3, shift0_6, shift0_7);
+      //printf("Performance timer: %i ms\n", (end_b-start_b)/1000);
+
+      // for (int i = 0; i < 2048; i++) {
+      //   //printf("%i;%f;%f;%f\n", i, adc0buff[i], real_fft_plan0->output[i], real_ifft_plan0->output[i]);
+      //   printf("%i;%f;%f;%f;%f;%f;%f;%f\n", i, adc0buff[i], real_fft_plan0->output[i], real_ifft_plan0->output[i],   adc3buff[i], real_fft_plan1->output[i], real_ifft_plan1->output[i], real_ifft_foldning->output[i]);
+      // }
+
+      printf("%f samples\t%i ms\n", shift0_3, (end_b-start_b)/1000);
+    }
+  }
+}

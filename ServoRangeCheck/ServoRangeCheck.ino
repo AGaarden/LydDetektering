d748617b0c485df55b3ad261ccd6404698c5a1b6
Sander Gaarden <sandergaarden@gmail.com>
2021-05-19 13:13:48 +0200

    Updated to match code convention.

--- ServoRangeCheck/ServoRangeCheck.ino
+++ ServoRangeCheck/ServoRangeCheck.ino
@@ -1,33 +1,33 @@
-const int servoPin = 19;  /* GPIO19 */
+const int SERVO_PIN_1 = 19;  /* GPIO19 */
 const int SERVO_PIN_2 = 18; /* GPIO18 */
 
-int dutyCycle = 0;
+int duty_cycle = 0;
 
 /* Setting PWM properties */
-const double PWMFreq = 50;
-const int PWMChannel1 = 0; /* Zero indexed */
-const int PWMChannel2 = 1; /* Zero indexed */
-const int PWMResolution = 11;
-//const int MAX_DUTY_CYCLE = (int)(pow(2, PWMResolution) - 1);
+const double PWM_FREQ = 50;
+const int PWM_CHANNEL_1 = 0; /* Zero indexed */
+const int PWM_CHANNEL_2 = 1; /* Zero indexed */
+const int PWM_RESOLUTION = 11;
+//const int MAX_DUTY_CYCLE = (int)(pow(2, PWM_RESOLUTION) - 1);
 
 void setup() {
   Serial.begin(115200);
-  ledcSetup(PWMChannel1, PWMFreq, PWMResolution);
-  ledcSetup(PWMChannel2, PWMFreq, PWMResolution);
+  ledcSetup(PWM_CHANNEL_1, PWM_FREQ, PWM_RESOLUTION);
+  ledcSetup(PWM_CHANNEL_2, PWM_FREQ, PWM_RESOLUTION);
   /* Attach the LED PWM Channel to the GPIO Pin */
-  ledcAttachPin(servoPin, PWMChannel1);
-  ledcAttachPin(SERVO_PIN_2, PWMChannel2);
-  ledcWrite(PWMChannel1, dutyCycle);
-  ledcWrite(PWMChannel2, dutyCycle);
+  ledcAttachPin(SERVO_PIN_1, PWM_CHANNEL_1);
+  ledcAttachPin(SERVO_PIN_2, PWM_CHANNEL_2);
+  ledcWrite(PWM_CHANNEL_1, duty_cycle);
+  ledcWrite(PWM_CHANNEL_2, duty_cycle);
 }
 
 void loop() {
   while(Serial.available()) {
     String in_char = Serial.readStringUntil('\n');
-    dutyCycle = in_char.toInt();
-    Serial.println(dutyCycle);
-    ledcWrite(PWMChannel1, dutyCycle);
-    ledcWrite(PWMChannel2, dutyCycle + 20);
+    duty_cycle = in_char.toInt();
+    Serial.println(duty_cycle);
+    ledcWrite(PWM_CHANNEL_1, duty_cycle);
+    ledcWrite(PWM_CHANNEL_2, duty_cycle);
     delay(10);
   }
 }

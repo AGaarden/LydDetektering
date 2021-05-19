6ccbf3398e35a56bdda4b20b87c8a3c06f33df9a
StephanAAU <70698913+StephanAAU@users.noreply.github.com>
2021-05-19 13:33:34 +0200

    Creating a backup of latest

---
+++ FFTTimeshift_Camera_highSample_backup/lib/Camera/Camera.h
@@ -0,0 +1,68 @@
+/*
+ * Camera.h - Library for moving a camera with a set of two servos
+ * Created by Sander Gaarden, April 26, 2021
+ */
+
+/* RESOLUTION RANGE LOOKUP TABLE (From 0-180 on MG996R Servo motors. Parentheses are minimum and maximum outside of 0-180 range)
+ * 8 bit: 5-30 (5-33)
+ * 10 bit: 20-119 (18-134)
+ * 11 bit: 39-238 (36-268) (+5 for map, 2.1%)
+ * 12 bit: 78-477 (71-537) (+5 for map, 1%)
+ * 16 bit: 1220-7600 (1134-8600) (+152 for map, 2%)
+ */
+
+/* Include necessary libraries */
+#include <esp32-hal-ledc.h>
+#include <Esp.h>
+#include <stdint.h>
+
+#ifndef Camera_h
+#define Camera_h
+
+// make a structure with the necessary angles to point towards an occurrence
+typedef struct {
+  double xAngle;
+  double yAngle;
+} angleSet;
+
+class Camera{
+  private:
+    /* PWM properties */
+    static const uint8_t BASE_PWM_FREQ = 50;
+    static const uint8_t PWM_CHANNEL_1 = 0; /* Zero indexed */
+    static const uint8_t PWM_CHANNEL_2 = 1; /* Zero indexed */
+    static const uint8_t PWM_RESOLUTION = 11;
+
+    /* Values based on PWM_RESOLUTION, check lookup table */
+    static const uint16_t PWM_MIN = 39;
+    static const uint16_t PWM_MAX = 238;
+    static const uint8_t PWM_MAP_EXTRA = 5; /* Arbitrary value for getting closer to 180 degrees when using map */
+
+    /* Base servo pins */
+    static const uint8_t BASE_SERVO_PIN_1 = 19;
+    static const uint8_t BASE_SERVO_PIN_2 = 18;
+
+    /* The current position of the camera gets saved to these variables */
+    uint8_t current_servo_pos_1;
+    uint8_t current_servo_pos_2;
+
+    /* Base variable for plusminus calculations */
+    static const uint8_t BASE_ANGLE_VARIATION = 5;
+
+    /* The variables that get set by constructor */
+    uint8_t _pwm_freq;
+    uint8_t _servo_pin_1;
+    uint8_t _servo_pin_2;
+    uint8_t _angle_variation;
+
+    /* The functions */
+    uint16_t angleToPwm(uint8_t angle);
+    uint8_t pwmToAngle(uint16_t pwm);
+  public:
+    /* The functions */
+    Camera(uint8_t servo_pin_1 = BASE_SERVO_PIN_1, uint8_t servo_pin_2 = BASE_SERVO_PIN_2, uint16_t period_hertz = BASE_PWM_FREQ, uint8_t angle_variation = BASE_ANGLE_VARIATION);
+    bool move(angleSet angles);
+    void testServos();
+};
+
+#endif
---
+++ FFTTimeshift_Camera_highSample_backup/lib/direction/direction.cpp
@@ -0,0 +1,149 @@
+#include <math.h>
+#include "direction.h"
+#include <stdio.h>
+#include <Camera.h>
+
+// double yk(double x, double da, double l){
+//     return (1/2.0)*sqrt( ( (4*pow(x-(0+l/2),2)-pow(da,2))*(pow(l,2)-pow(da,2)) ) / (pow(da,2.0)) );
+// }
+
+Point direction(double ta, double tb, double tc){
+    angleSet temp;
+    Point temp_point;
+    int c = 343;
+    double da = c*ta;
+    double db = c*tb;
+    double dc = c*tc;
+    double l = 0.135;
+    double x;
+    double y;
+    double z;
+    double yk;
+    double dist;
+    double theta;
+    double max_delay = l / c * 0.995;
+    double theta_check;
+    //printf("Max delay: %f\n", max_delay);
+    if (abs(ta) > max_delay) {
+        // Return no point
+        return Point{NAN, NAN, NAN};
+    // } else if ((da < 0) && (db > 0)) {
+    //     x = (pow(l,3)*pow(da,2) + pow(l,3)*pow(db,2) - 2*l*pow(da,2)*pow(db,2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2))) + sqrt(pow(da,2)*pow(db,2)*pow(l,2)*pow(-2*pow(l,2) + pow(da,2) + pow(db,2.0),2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2)));
+    // } else if ((da > 0) && (db < 0)) {
+    //     x = (pow(l,3)*pow(da,2) + pow(l,3)*pow(db,2) - 2*l*pow(da,2)*pow(db,2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2))) - sqrt(pow(da,2)*pow(db,2)*pow(l,2)*pow(-2*pow(l,2) + pow(da,2) + pow(db,2.0),2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2)));
+    // } else {
+    //     x = 0;
+    // }
+    } else if ((da == db) || ((abs(da - db)) < 1e-6)) {
+        x = 0;
+    } else if (da*db < 0) {
+        x = (pow(l,3)*pow(da,2) + pow(l,3)*pow(db,2) - 2*l*pow(da,2)*pow(db,2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2))) + sqrt(pow(da,2)*pow(db,2)*pow(l,2)*pow(-2*pow(l,2) + pow(da,2) + pow(db,2.0),2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2)));
+    } else {
+        x = (pow(l,3)*pow(da,2) + pow(l,3)*pow(db,2) - 2*l*pow(da,2)*pow(db,2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2))) - sqrt(pow(da,2)*pow(db,2)*pow(l,2)*pow(-2*pow(l,2) + pow(da,2) + pow(db,2.0),2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2)));
+    }
+
+    yk = (1/2.0)*sqrt( ( (4*pow(x-(0+l/2),2)-pow(da,2))*(pow(l,2)-pow(da,2)) ) / (pow(da,2.0)) );
+
+    //dist = sqrt(pow(yk(x, da, l),2)+pow(x,2));
+    dist = sqrt(pow(yk,2)+pow(x,2));
+
+    // printf("pow(l, 2): %f\n", pow(l, 2));
+    // printf("pow(dc, 2): %f\n", pow(dc, 2));
+    // printf("2*dc*dist: %f\n", 2*dc*dist);
+    z = (pow(l,2) - pow(dc,2) - 2*dc*dist) / (2*l);
+
+    //printf("yk(x,da,l): %f\n", yk(x,da,l));
+    //printf("z/yk(x,da,l): %f\n", z/yk(x,da,l));
+    //theta_check = z/yk(x,da,l);
+    theta_check = z/yk;
+
+    if (theta_check > 1) {
+        theta_check = 1;
+    } else if (theta_check < -1) {
+        theta_check = -1;
+    }
+    theta = asin(theta_check);
+    if (isnan(theta)) {
+        //printf("Theta is nan: %f\n", z/yk(x,da,l));
+        printf("Theta is nan: %f\n", z/yk);
+    }
+    //printf("theta: %f\n", theta);
+    //y = yk(x,da,l)*cos(theta);
+    y = yk*cos(theta);
+
+    //test
+    //printf("%f\t %f\t %f\n", x, y, z);
+    return Point {x, y, z};
+}
+
+angleSet direction_angle(double ta, double tb, double tc){
+    angleSet temp;
+    double x;
+    double y;
+    double z;
+    double xOffset = 0;
+    double yOffset = 0;
+    double zOffset = 0;
+    double kDist;
+
+    Point coords = direction(ta, tb, tc);
+
+    x = coords.x;
+    y = coords.y;
+    z = coords.z;
+
+    kDist = sqrt(pow(x-xOffset,2)+pow(y-yOffset,2)+pow((z-zOffset),2));
+
+    temp.xAngle = acos((x - xOffset)/sqrt(pow(x - xOffset,2)+pow(y - yOffset,2)))*(180/M_PI);
+
+    if (z < 0) {
+        temp.yAngle = -acos(sqrt(pow(x - xOffset ,2)+pow(y - yOffset,2))/kDist)*(180/M_PI)+90;
+    } else {
+        temp.yAngle = acos(sqrt(pow(x - xOffset,2)+pow(y - yOffset,2))/kDist)*(180/M_PI)+90;
+    }
+    
+    //test
+    //printf("%f\t %f\t %f\n", x, y, z);
+    return temp;
+}
+
+/*
+// Old method without sanity checks:
+// angleSet direction_angle(double ta, double tb, double tc){
+//     angleSet temp;
+//     int c = 343;
+//     double da = c*ta;
+//     double db = c*tb;
+//     double dc = c*tc;
+//     double l = 0.15;
+//     double x;
+//     double y;
+//     double z;
+//     double xOffset = 0;
+//     double yOffset = 0;
+//     double zOffset = 0.20;
+//     double kDist;
+//     double dist;
+//     double theta;
+//     if (da*db < 0){
+//         x = (pow(l,3)*pow(da,2) + pow(l,3)*pow(db,2) - 2*l*pow(da,2)*pow(db,2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2))) + sqrt(pow(da,2)*pow(db,2)*pow(l,2)*pow(-2*pow(l,2) + pow(da,2) + pow(db,2.0),2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2)));
+//     } else {
+//         x = (pow(l,3)*pow(da,2) + pow(l,3)*pow(db,2) - 2*l*pow(da,2)*pow(db,2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2))) - sqrt(pow(da,2)*pow(db,2)*pow(l,2)*pow(-2*pow(l,2) + pow(da,2) + pow(db,2.0),2))/(2*pow(l,2)*(-pow(da,2) + pow(db,2)));
+//     }
+    
+//     dist = sqrt(pow(yk(x, da, l),2)+pow(x,2));
+
+//     z = (pow(l,2) - pow(dc,2) - 2*dc*dist) / (2*l);
+
+//     theta = asin(z/yk(x,da,l));
+
+//     y = yk(x,da,l)*cos(theta);
+
+//     kDist = sqrt(pow(x+xOffset,2)+pow(y+yOffset,2)+pow((z+zOffset),2));
+
+//     temp.xAngle = acos(x/sqrt(pow(x,2)+pow(y,2)))*(180/M_PI);
+//     temp.yAngle = acos(sqrt(pow(x,2)+pow(y,2))/kDist)*(180/M_PI)+90;
+//     //test
+//     return temp;
+// }
+*/
